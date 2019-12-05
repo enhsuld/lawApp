45e9dafae069eeff2ca4a61e3186a2793a0d9720
hatnaa <hatnaa.k@gmail.com>
2019-12-05 12:50:48 +0800

    sss

--- lib/services/BackendService.dart
+++ lib/services/BackendService.dart
@@ -7,29 +7,28 @@ import 'package:law_app/models/term.dart';
 
 class BackendService {
   //static String apiURL = "http://192.168.1.116:80/api/v1";
- // static String url = "http://192.168.1.111:8080/api";
+  // static String url = "http://192.168.1.111:8080/api";
   static String url = "http://goldwatch.ml/api";
   static String apiURL = url + "/cnt";
 
   static Future<List<TermModel>> getContent(offset, limit,
       {publish: '', filters: ''}) async {
-
-    final response = (await http.get(
-        apiURL + '/term/term/all'));
+    final response = (await http.get(apiURL + '/term/term/all'));
     print(response.statusCode);
     print(json.decode(response.body));
     return TermModel.fromJsonList(json.decode(response.body));
   }
 
   static Future<List<TermModel>> getContentById(id) async {
-    final response = (await http.get(apiURL + '/term/list/'+id.toString()));
+    final response = (await http.get(apiURL + '/term/list/' + id.toString()));
     print(response.statusCode);
     print(json.decode(response.body));
     return TermModel.fromJsonList(json.decode(response.body));
   }
 
   static Future<List<TaxonomyModel>> getTaxonomyById(id) async {
-    final response = (await http.get(apiURL + '/term/taxonomy/list/'+id.toString()));
+    final response =
+        (await http.get(apiURL + '/term/taxonomy/list/' + id.toString()));
     print(response.statusCode);
     print(json.decode(response.body));
     return TaxonomyModel.fromJsonList(json.decode(response.body));
