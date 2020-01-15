33efeb22d1f4fe053c8fe0cfe642bc0cc0cea39c
hatnaa <hatnaa.k@gmail.com>
2020-01-15 16:30:34 +0800

    launcher icon and launch image change

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:law_app/items/TermItem.dart';
 import 'package:law_app/models/term1.dart';
 import 'package:law_app/services/BackendService.dart';
+import 'package:law_app/utils/colorlaw.dart';
 
 class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
@@ -26,61 +27,62 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: ColorLaw.blue,
         body: Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/images/Home.png"),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Column(
-        children: <Widget>[
-          Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  height: 260,
-                  padding:
-                      EdgeInsets.only(top: 80, left: 50, right: 50, bottom: 10),
-                  child: Image.asset("assets/images/logo_large.png"),
-                ),
-                Container(
-                  padding:
-                      EdgeInsets.only(top: 15, left: 50, right: 50, bottom: 10),
-                  child: Text(
-                    'МОНГОЛ УЛСЫН ҮНДСЭН ХУУЛЬ',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 21.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: 20, bottom: 10),
-                  child: Image.asset("assets/images/hee_home.png"),
-                ),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.5,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                  child: ListView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: publishedTerms.length,
-                    itemBuilder: (ctx, i) {
-                      final item = publishedTerms[i];
-                      return TermItem(item, i);
-                    },
-                  ),
-                ),
-              ],
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/Home.png"),
+              fit: BoxFit.cover,
             ),
           ),
-        ],
-      ),
-    ));
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 260,
+                      padding: EdgeInsets.only(
+                          top: 80, left: 50, right: 50, bottom: 10),
+                      child: Image.asset("assets/images/logo_large.png"),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(
+                          top: 15, left: 50, right: 50, bottom: 10),
+                      child: Text(
+                        'МОНГОЛ УЛСЫН ҮНДСЭН ХУУЛЬ',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 21.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 20, bottom: 10),
+                      child: Image.asset("assets/images/hee_home.png"),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.5,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 5),
+                      child: ListView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: publishedTerms.length,
+                        itemBuilder: (ctx, i) {
+                          final item = publishedTerms[i];
+                          return TermItem(item, i);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
